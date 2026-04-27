2f6dd6b42956693a28650043f3ce953155de5ccb
Hesham <155351604+hesh-7@users.noreply.github.com>
2026-04-27 21:33:18 +0600

    Create numberguess.py

---
+++ numberguess.py
@@ -0,0 +1,22 @@
+from random import randint
+random_number = randint(1,100)
+tries = 0
+while True:
+    try:
+        if tries==0:
+            user_guess = int(input("Guess a number between 1 to 100 : "))
+        else:
+            user_guess = int(input("Guess_Again : "))
+        tries +=1
+    except:
+        print("Invalid Input\nProgramme Exitted!")
+        quit()
+    if user_guess == random_number :
+        print("That was a perfect guess!")
+        break
+    elif user_guess < random_number:
+        print("The number is greater than",user_guess)
+    elif user_guess > random_number:
+        print("The number is less than",user_guess)
+print("It took you",tries,"tries to finish!")
+    
