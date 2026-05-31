e3de7c6ddfd9787a226290fd4d91c7a576d8b6f0
Hesham <155351604+hesh-7@users.noreply.github.com>
2026-05-31 20:39:38 +0600

    Update stringindexing.py

--- stringindexing.py
+++ stringindexing.py
@@ -1,4 +1,5 @@
 #StringIndexing
+# start : end : step
 
 card = "1234-5678-9012-3456"
 print(card[0])
@@ -6,3 +7,6 @@ print(card[1])
 print(card[-1])
 print(card[0:4])#Last No get excluded
 print(card[:-2])
+print(card[::2])#every second character
+print(card[-4:])
+print(f"xxxx-xxxx-xxxx-{card[-4:]}")
